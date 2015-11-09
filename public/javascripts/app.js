be05fcabfe7728868003f0e223290d987db501a3
Nicolas <nicolasrn@hotmail.com>
2015-11-09 10:58:21 +0100

    maj du trie

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,24 +1,36 @@
 (function($) {
 	$(function() {
-		var template = $('table.recherche').find('tbody').children();
-		
 		function getQuery () {
 			var champ = $('input[name=query]');
 			return function() {
 				return champ.val();
-			}
+			};
 		}
 		
-		function rafraichir(query) {
+		function getSort () {
+			var champ = $('table.recherche a[data-selected]');
+			return function() {
+				var tab = [];
+				if (champ.attr('id')) {
+					tab.push(champ.attr('id'));
+				}
+				if (champ.attr('data-selected')) {
+					tab.push(champ.attr('data-selected'));
+				}
+				return tab;
+			};
+		}
+		
+		function rafraichir(query, sort) {
 			$.ajax({
 				method : "get",
 				url : "/search",
 				data : {
-					q:query
+					q:query,
+					s:sort
 				}
 			}).success(function(data) {
-				$('table.recherche').find('tbody').children().remove();
-				$('table.recherche').find('tbody').append(template);
+				$('table.recherche').find('tbody').children().not(':first').remove();
 				$('table.recherche').find('tbody').append($.map(data, function (item) {
 					var ligne = $('<tr>');
 					ligne.append($('<td>').text(item.serie));
@@ -31,11 +43,33 @@
 			});
 		};
 		
-		rafraichir(getQuery());
-		
 		$('input[name=query]').on('keyup', function(event) {
-			rafraichir(getQuery());
+			rafraichir(getQuery(), getSort());
 		});
 		
+		$('table.recherche a').each(function(index, item) {
+			$(this).on('click', function(event) {
+				var a= $(this);
+				a.parents('tr').find('a').each(function(index, item) {
+					if (a.attr('id') != $(item).attr('id')) {
+						$(item).removeAttr('data-selected');
+					}
+				});
+				if (!a.attr('data-selected')) {
+					a.attr('data-selected', 'down');
+				} else {
+					if (a.attr('data-selected') === 'down') {
+						a.attr('data-selected', 'up');
+					} else {
+						a.attr('data-selected', 'down');
+					}
+				}
+				
+				var champ = champ = $('table.recherche a[data-selected]');
+				rafraichir(getQuery(), getSort());
+			});
+		});
+		
+		rafraichir(getQuery(), getSort());
 	});
 })(jQuery);
--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,77 @@ var router = express.Router();
 var fs = require('fs');
 var _ = require("underscore");
 
+function normalSort(champ, ordre) {
+	return function(a, b) {
+		var retour = null;
+		if (a[champ] == b[champ])
+			return 0;
+		if (a[champ] > b[champ]) {
+			retour = 1;
+		} else {
+			retour = -1;
+		}
+		return retour * ordre; 
+	}
+}
+
+function naturalSort (champ, ordre) {
+	return function naturalSort2(as, bs){
+		var rx=/(\d+)|(\D+)/g, rd=/\d+/;
+	    var a= String(as[champ]).toLowerCase().match(rx);
+	    var b= String(bs[champ]).toLowerCase().match(rx);
+	    var retour = null;
+	    while(a.length && b.length){
+	        var a1= a.shift();
+	        var b1= b.shift();
+	        if(rd.test(a1) || rd.test(b1)){
+	            if(!rd.test(a1)) return 1 * ordre;
+	            if(!rd.test(b1)) return -1 * ordre;
+	            if(a1!= b1) return (a1 - b1) * ordre;
+	        }
+	        else if(a1!= b1) return a1 > b1 ? 1 : -1 * ordre;
+	    }
+	    return (a.length- b.length) * ordre;
+	}
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', {
-		title : 'Let\'s go',
-		query: req.query.q
+		title: 'Let\'s go',
+		query: req.query.query
 	});
 });
 
 router.get('/search', function(req, res, next) {
-	param = req.query.q;
+	var queryParam = req.query.q || '';
+	var sortParam = req.query.s || '';
+	
 	fs.readFile('./public/ressources/livre.json', 'utf8', function(err, data) {
 		if (err) {
 			return console.log(err);
 		}
-		data = JSON.parse(data);
-
-		data = _.filter(data, function (item) {
+		var data = JSON.parse(data);
+		var sort = data.config;
+		
+		data = _.filter(data.livre, function (item) {
 			var stringify = JSON.stringify(item);
-			return stringify.indexOf(param) > -1;
+			return stringify.indexOf(queryParam) > -1;
 		});
+		
+		if (sortParam) {
+			sortParam = sortParam.split(',');
+			var champ = sortParam[0];
+			var ordre = sortParam[1] == 'down' ? -1 : 1;
+			
+			data = data.sort(function(champ, ordre, sort) {
+				if (sort[champ] == 'normalSort') {
+					return normalSort(champ, ordre);
+				} else if (sort[champ] == 'naturalSort') {
+					return naturalSort(champ, ordre);
+				}
+			}(champ, ordre, sort));
+		}
 		
 		res.set('Content-Type', 'text/json');
 		res.send(data);
